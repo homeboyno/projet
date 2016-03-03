4679c69c72ee45d478b24728a641449afbdbff51
nicolas.marty <nicolas.marty@mass-cara2.mathinfo.i-univ-tlse2.fr>
2016-03-03 18:33:10 +0100

    Modif classe Teleporteur : ajout fichier enregistrement trace

--- Teleporteur.py
+++ Teleporteur.py
@@ -1,6 +1,7 @@
 '''from Voiture import Voiture
 from Place import Place'''
 from Placement import Placement 
+from datetime import datetime
 
 class Teleporteur:
 	def __init__(self, tel):
@@ -11,10 +12,14 @@ class Teleporteur:
 		voiture.addPlacementV(placement)
 		place.addPlacementP(placement)
 		print("Votre voiture est bien prise en charge. Place: "+ str(place.idPlace) +". Bonne journee !")
+		with open('TracePassage.txt', 'a') as fichier:
+			fichier.write(str(datetime.now()) + ": Entree du vehicule : " + str(voiture.immatriculation) + ". Place occupe : " + str(place.idPlace) +".\n")
 		
 	def teleporterVoitureSuperAbonne(self, voiture):
 		if(Parking.recherchePlace(voiture) == None):
 			print("Votre voiture est bien prise en charge. Bonne journee !")
+			with open('TracePassage.txt', 'a') as fichier:
+				fichier.write(str(datetime.now()) + ": Entree du vehicule super Abonne : " + str(voiture.immatriculation) + "\n")
 		else:
 			teleporterVoiture(voiture, Parking.recherchePlace(voiture))
 		
@@ -24,7 +29,9 @@ class Teleporteur:
 				pla.placement.partirPlace()
 				parking.nbPlacesLibres += 1 
 				print("Veuillez patienter le temps que votre vehicule arrive.")
-				break
+				with open('TracePassage.txt', 'a') as fichier:
+					fichier.write(str(datetime.now()) + ": Sortie du vehicule : " + str(pla.placement.voiture.immatriculation) + ". Place occupe : " + str(pla.idPlace) +".\n")
+				return
 		print("La place demander est errone")
 		
 #place = Recuperer une place existante avec Parking.rechercherPlace.
